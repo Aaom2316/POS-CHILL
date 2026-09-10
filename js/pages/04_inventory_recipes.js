bc8ad5a1dd8107ec914bcfe2aff3344c80d1c78d
Aaom2316 <96713764+Aaom2316@users.noreply.github.com>
2026-09-10 18:32:29 +0700

    Update 04_inventory_recipes.js

--- js/pages/04_inventory_recipes.js
+++ js/pages/04_inventory_recipes.js
@@ -30,7 +30,7 @@ POS.pages.inventoryRecipes = async function(){
       ">
         <div>
           <h1 class="page-title" style="margin:0 0 5px;font-size:30px;font-weight:800;color:#1f2937;">
-            🍳 สูตร
+            🍳 Recipes
           </h1>
           <p class="page-subtitle" style="margin:0;">
             สูตรอาหารและการใช้วัตถุดิบ
